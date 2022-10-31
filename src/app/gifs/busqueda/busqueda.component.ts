ec8db46207a8ffa2473a29dc40aef70eb16780e8
Luis Angel Torres Grimaldo <t.g.luisangel@hotmail.com>
2022-10-31 15:48:02 -0600

    save modules

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,20 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+})
+export class BusquedaComponent {
+
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  constructor(private gifsService: GifsService) { }
+
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+    this.gifsService.buscarGifs(valor);
+    this.txtBuscar.nativeElement.value = '';
+
+  }
+}
---
+++ src/app/gifs/resultados/resultados.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Gif } from '../interfaces/gifs.interface';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-resultados',
+  templateUrl: './resultados.component.html',
+})
+export class ResultadosComponent {
+  get resultados(): Gif[] {
+    return this.gifsService.resultados;
+  }
+  constructor(private gifsService: GifsService) { }
+}
